5d8d3674e522591f32cc01b5a75a7e9acaa9c662
Himansh29 <himanshubhange886@gmail.com>
2024-05-28 17:25:20 +0530

    add changes to B-2

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,8 +7,12 @@ class HelloWorld{
     public static void main(String []args) {
         System.out.println("Hello world");
 
+        System.out.println("Made some changes on Branch 2 (i.e B1)");
+
+
         System.out.println("Made some changes on Branch 2 (i.e B2)");
-        greetuser("Himanshu");
+        greetuser('Himanshu');
+
 
     }
 }
