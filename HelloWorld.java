ad25990317eae090eb86109192e476c1efaab1be
Himansh29 <himanshubhange886@gmail.com>
2024-05-28 17:19:45 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld{
+
+    static void greetuser(String name) {
+        System.out.println("Hello There " + name);
+    }
+
+    public static void main(String []args) {
+        System.out.println("Hello world");
+
+        System.out.println("Made some changes on Branch 2 (i.e B2)");
+        greetuser("Himanshu");
+
+    }
+}
